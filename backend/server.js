51ce8fb23e4e621e6e58322cfdb4323dc2599aca
parshik04 <parshikchovatiya14@gmail.com>
2025-08-15 01:01:07 +0530

    server.js is added

---
+++ backend/server.js
@@ -0,0 +1,42 @@
+const express = require("express")
+const helmet = require("helmet")
+const morgan = require("morgan")
+const cors = require("cors")
+
+require("dotenv").config();
+const app = express()
+
+
+// middlewares
+app.use(cors(),morgan("combined"),helmet())
+app.disable("x-powered-by")
+
+
+//Routes
+app.get("/",(req,res)=>{
+    res.status(200).send("server us started")
+})
+
+
+//Global error handler
+app.use((err,req,res,next)=>{
+    console.error("Error : ", err.stack)
+    res.status(500).json({ error: "Something went wrong!" });
+})
+
+
+//start server
+app.listen(process.env.PORT || 8080,()=>{
+    console.log("server is stareted");
+})
+
+//handle unhandled promises and rejection
+process.on("unhandledRejection",(reason,promise)=>{
+    console.error("unhandle Rejection : ",reason)
+    process.exit(1)
+})
+
+process.on("uncaughtException", (err) => {
+    console.error("Uncaught Exception:", err);
+    process.exit(1);
+});
